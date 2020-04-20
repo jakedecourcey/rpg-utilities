e3f35ca5503ed98075458491425a5c31fa9c51f3
Jake DeCourcey <33670605+jakedecourcey@users.noreply.github.com>
2020-04-19 23:06:09 -0500

    Added basic db connection

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // server.js
 
-const express = require ('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const connectionString = 'mongodb://192.168.50.5:27017';
+
 const app = express ();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function(){
       console.log('listening on 3000');
@@ -9,4 +16,13 @@ app.listen(3000, function(){
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/add', (req, res) => {
+    console.log(req.body);
+});
+
+MongoClient.connect(connectionString, (err, client) => {
+    if (err) return console.error(err);
+    console.log('Connected to Database');
 });
